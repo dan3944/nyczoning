92fc7c7c88a5374f7964d9ebb3cd6cdaec909ff5
Dan Cohen <dccohe@gmail.com>
2023-07-04 11:29:43 -0400

    directories

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ export ZONING_DB_PATH=database.db
 if [ ! -f $ZONING_DB_PATH ]
 then
     echo "creating $ZONING_DB_PATH"
-    cat db/schema.sql | sqlite3 $ZONING_DB_PATH
+    cat src/schema.sql | sqlite3 $ZONING_DB_PATH
 fi
 
 source bin/activate

760c1a2cd38dd8b201cda2de5ba6a1fe77ac7505
Dan Cohen <dccohe@gmail.com>
2023-07-08 10:25:38 -0400

    write to logfile

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
+import argparse
+import datetime as dt
 import logging
+import os
+
 import download
 import notify
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(prog='NYC Zoning Notifier')
+    parser.add_argument('-l', '--logdir', help='Directory to store log files')
+    args = parser.parse_args()
+
+    if args.logdir:
+        os.makedirs(args.logdir, exist_ok=True)
+        logfile = f'{args.logdir}/{dt.datetime.now().isoformat()}.log'
+    else:
+        logfile = None
+
+    logging.basicConfig(level=logging.INFO, filename=logfile)
     download.download_pdfs()
     notify.notify_meetings()
